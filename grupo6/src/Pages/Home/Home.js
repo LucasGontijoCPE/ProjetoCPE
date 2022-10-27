613d53118336c0b9fffcb03177bc6f43c6fe5f6a
Thiago Fraga <fraga.thiagoribeiro@gmail.com>
2022-10-27 10:47:17 -0300

    Mudanças no Header
    Mudanças no header

--- grupo6/src/Pages/Home/Home.js
+++ grupo6/src/Pages/Home/Home.js
@@ -6,21 +6,27 @@ function Home() {
     return (
     <div>
         <div className='Head'>
-            <div className='MenuBar'>
-                <div>
-                    <img src='/imagens/LOGO.png' alt='LOGO'/>
+            <div className='HeadItem'>
+                    <img src='/imagens/LOGO.png' alt='LOGO' />
+            </div>
+            <div className='HeadItem grow2'>
+                <div className='MenuBar'>
+                    <h2 className='MenuTool textos' >Menu</h2>
+                    <h2 className='MenuTool textos'>Produtos</h2>
+                    <h2 className='MenuTool textos'>Sobre Nós</h2>
+                    <div className='Login MenuTool'>
+                        <img src='/imagens/Login.png' alt='Login'/>
+                        <h2 className='textos'>Entre ou Cadastre-se</h2>
+                    </div>
+                    
                 </div>
-                <h2 className='MenuTool'>Menu</h2>
-                <h2 className='MenuTool'>Produtos</h2>
-                <h2 className='MenuTool'>Sobre Nós</h2>
-                <div className='Login'>
-                    <img src='/imagens/Login.png' alt='Login'/>
-                    <h2 className='MenuTool'>Entre ou Cadastre-se</h2>
-                </div>
-                
             </div>
             
+
+            
         </div>
+
+
         <h1>Home</h1>
         <Link to="login">Login</Link>
         <Link to="perfil">Perfil</Link>
