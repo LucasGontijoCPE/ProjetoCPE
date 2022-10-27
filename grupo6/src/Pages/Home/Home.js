f2dd6fa0f4ed70011764528726487ae95f4f58df
Thiago Fraga <fraga.thiagoribeiro@gmail.com>
2022-10-26 22:15:17 -0300

    Alteração header da homepage
    Alteração da cor do background, criação dos itens do menu

--- grupo6/src/Pages/Home/Home.js
+++ grupo6/src/Pages/Home/Home.js
@@ -6,11 +6,20 @@ function Home() {
     return (
     <div>
         <div className='Head'>
-            <div id="Logo">
-                <img src='/imagens/LOGO.png' alt='LOGO'/>
+            <div className='MenuBar'>
+                <div>
+                    <img src='/imagens/LOGO.png' alt='LOGO'/>
+                </div>
+                <h2 className='MenuTool'>Menu</h2>
+                <h2 className='MenuTool'>Produtos</h2>
+                <h2 className='MenuTool'>Sobre Nós</h2>
+                <div className='Login'>
+                    <img src='/imagens/Login.png' alt='Login'/>
+                    <h2 className='MenuTool'>Entre ou Cadastre-se</h2>
+                </div>
+                
             </div>
-            <h2>Menu</h2>
-            <h2>Entre ou Cadastre-se</h2>
+            
         </div>
         <h1>Home</h1>
         <Link to="login">Login</Link>
