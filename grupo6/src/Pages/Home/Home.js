c3ed423438bf81decab69c40d4274fedaf526270
Thiago Fraga <fraga.thiagoribeiro@gmail.com>
2022-11-01 15:34:50 -0300

    Adicionado o background à página

--- grupo6/src/Pages/Home/Home.js
+++ grupo6/src/Pages/Home/Home.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import Head from './Head.js';
 import "./Home.css"
 
 function Home() {
     
     return (
     <div>
-        <Head/>
 
 
         <h1>Home</h1>
         <Link to="login">Login</Link>
         <Link to="perfil">Perfil</Link>
         <Link to="produtos">Produtos</Link>
+
+        <div className='Blocos bl1'>
+
+        </div>
+        <div className='Blocos bl2'>
+            
+        </div>
+
+
 
     </div>
     
