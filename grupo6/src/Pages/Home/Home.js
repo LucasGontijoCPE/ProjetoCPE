df6a8de048ca5f89398dbc2e2dbccc4311174708
Thiago Fraga <fraga.thiagoribeiro@gmail.com>
2022-11-03 16:03:10 -0300

    Página Home Finalizada
    Finalização da página home responsiva

--- grupo6/src/Pages/Home/Home.js
+++ grupo6/src/Pages/Home/Home.js
@@ -14,10 +14,23 @@ function Home() {
         <Link to="produtos">Produtos</Link>
 
         <div className='Blocos bl1'>
-
+            <div className='bl1-linha'>
+                <div className='texto-produtos'>
+                    <h2 className='textos'>Central de Produtos Esportivos</h2>
+                    <p className='textos desktop'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua.  Elit ut aliquam purus sit amet luctus venenatis lectus. </p>
+                    <Link to="produtos" className='Produtosbtn textos'>Produtos</Link>
+                </div>
+            </div>
         </div>
-        <div className='Blocos bl2'>
-            
+        <div className='Blocos ofertas'>
+            <h2 className='textos'>Ofertas Imperdíveis</h2>
+            <div className='vitrineOfertas'>
+                <div className='produtosOfertas'></div>
+                <div className='produtosOfertas'></div>
+                <div className='produtosOfertas'></div>
+                <div className='produtosOfertas'></div>
+            </div>
         </div>
 
 
